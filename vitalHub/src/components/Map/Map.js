1b6d4beaae309ddb29e8767280196a33cdba464d
Lucas Bianchezzi Oliveira <41365718875@info.local>
2024-03-13 15:05:32 -0300

    mapa

--- vitalHub/src/components/Map/Map.js
+++ vitalHub/src/components/Map/Map.js
@@ -19,91 +19,128 @@ import {
     LocationAccuracy // Precisão da captura
 } from 'expo-location';
 
-import { mapskey } from './utils/mapsKey';
+import { mapskey } from '../../../utils/mapsKey';
 
 export const Map = () => {
-    // Espera a permissão para a localização
-    const { granted } = await requestForegroundPermissionsAsync();
+    const [initialPosition, setInitialPosition] = useState(null);
+    const [finalPosition, setFinalPosition] = useState({
+        latitude: -23.667331,
+        longitude: -46.561491
+    });
 
-    if (granted) {
-        // Pega a localização atual do usuário
-        const currentPosition = await getCurrentPositionAsync();
+    const mapReference = useRef(null);
 
-        // Salva na constante
-        await setInitialPosition(currentPosition);
 
-        // Imprime
-        console.log(initialPosition);
+    async function capturarLocalizacao() {
+        // Espera a permissão para a localização
+        const { granted } = await requestForegroundPermissionsAsync();
+
+        if (granted) {
+            // Pega a localização atual do usuário
+            const currentPosition = await getCurrentPositionAsync();
+
+            // Salva na constante
+            await setInitialPosition(currentPosition);
+
+            // Imprime
+            console.log(initialPosition);
+        }
     }
-}
 
-async function RecarregarVisualizacaoMapa() {
-    // Se tivermos a referencia do mapa (ele iniciou) e o valor da posição inicial
-    if (mapReference.current && initialPosition) {
-        await mapReference.current.fitToCoordinates([
-            { latitude: initialPosition.coords.latitude, longitude: initialPosition.coords.longitude },
-            { latitude: finalPosition.latitude, longitude: finalPosition.longitude }
-        ], {
-            edgePadding: { top: 180, right: 60, left: 130, bottom: 60 },
-            animated: true
-        })
+    async function RecarregarVisualizacaoMapa() {
+        // Se tivermos a referencia do mapa (ele iniciou) e o valor da posição inicial
+        if (mapReference.current && initialPosition) {
+            await mapReference.current.fitToCoordinates([
+                { latitude: initialPosition.coords.latitude, longitude: initialPosition.coords.longitude },
+                { latitude: finalPosition.latitude, longitude: finalPosition.longitude }
+            ], {
+                edgePadding: { top: 30, right: 50, left: 50, bottom: 15 },
+                animated: true
+            })
+        }
     }
-}
 
-useEffect(() => {
-    capturarLocalizacao();
+    useEffect(() => {
+        capturarLocalizacao();
 
-    // Capturar localização em tempo real
-    watchPositionAsync({
-        accuracy: LocationAccuracy.High,
-        timeInterval: 1000,
-        distanceInterval: 1
-    }, async (response) => {
-        await setInitialPosition(response);
+        // Capturar localização em tempo real
+        watchPositionAsync({
+            accuracy: LocationAccuracy.High,
+            timeInterval: 1000,
+            distanceInterval: 1
+        }, async (response) => {
+            await setInitialPosition(response);
 
-        mapReference.current?.animateCamera({
-            pitch: 60,
-            center: response.coords
-        })
-    }
-    );
+            mapReference.current?.animateCamera({
+                pitch: 60,
+                center: response.coords
+            })
+        }
+        );
 
-}, [10000])
+    }, [10000])
 
-useEffect(() => {
-    RecarregarVisualizacaoMapa();
-}, [initialPosition])
+    useEffect(() => {
+        RecarregarVisualizacaoMapa();
+    }, [initialPosition])
 
-return (
-    <>
-        {initialPosition != null ? (
-            <MapDraw
-                initialRegion={{
-                    latitude: 45.4343,
-                    longitude: 12.3388,
-                    latitudeDelta: 0.005,
-                    longitudeDelta: 0.005,
-                }}
-            >
-                <MarkerPoint
-                    coordinate={{
-                        latitude: 45.4343,
-                        longitude: 12.3388,
-                    }}
-                    title='Exemplo de local'
-                    description='Descrição exemplo sobre o local'
-                    pinColor='blue'
-                />
-            </MapDraw>
-        ) : (
-            <>
-                <Text>Localização não encontrada!</Text>
-                {/* salve tuquinhas, muitas felicidades em irmão tmj pae */}
+    return (
+        <>
+            {initialPosition != null ?
+                (
+                    <MapDraw
+                        ref={mapReference}
+                        initialRegion={{
+                            latitude: 45.4343,
+                            longitude: 12.3388,
+                            latitudeDelta: 0.005,
+                            longitudeDelta: 0.005,
+                        }}
+                        provider={PROVIDER_GOOGLE}
+                    >
+                        <MapViewDirections
+                            origin={initialPosition.coords}
+                            destination={{
+                                latitude: finalPosition.latitude,
+                                longitude: finalPosition.longitude,
+                                latitudeDelta: 0.005,
+                                longitudeDelta: 0.005,
+                            }}
+                            apikey={mapskey}
 
-                <ActivityIndicator />
-            </>
-        )}
+                            strokeWidth={5}
+                            strokeColor='orange'
+                        />
 
-    </>
-)
+                        <MarkerPoint
+                            coordinate={{
+                                latitude: initialPosition.coords.latitude,
+                                longitude: initialPosition.coords.longitude,
+                            }}
+                            title='Exemplo de local'
+                            description='Descrição exemplo sobre o local'
+                            pinColor='blue'
+                        />
+
+                        <MarkerPoint
+                            coordinate={{
+                                latitude: -23.667331,
+                                longitude: -46.561491,
+                            }}
+                            title='Exemplo de local'
+                            description='Descrição exemplo sobre o local'
+                            pinColor='blue'
+                        />
+                    </MapDraw>
+                ) : (
+                    <>
+                        <Text>Localização não encontrada!</Text>
+                        {/* salve tuquinhas, muitas felicidades em irmão tmj pae */}
+
+                        <ActivityIndicator />
+                    </>
+                )}
+
+        </>
+    )
 }
--- vitalHub/src/components/Map/Style.js
+++ vitalHub/src/components/Map/Style.js
@@ -4,6 +4,7 @@ import MapView, { Marker } from "react-native-maps";
 export const MapDraw = styled(MapView).attrs({})`
     flex: 1;
     width: 100%;
+    margin-bottom: 10px;
 `;
 
 export const MarkerPoint = styled(Marker).attrs({})`
